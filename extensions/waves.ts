0deb50ce71b28334ad5c4cc194cc56c004113544
FKPC <fcplaysgamer@gmail.com>
2026-09-19 10:07:53 -0300

    Added more octaves and changed smth I wont remember

--- extensions/waves.ts
+++ extensions/waves.ts
@@ -2,6 +2,18 @@
 
 let labelArray = [
     "HH",
+    "C0",
+    "c0",
+    "D0",
+    "d0",
+    "E0",
+    "F0",
+    "f0",
+    "G0",
+    "g0",
+    "A0",
+    "a0",
+    "B0",
     "C1",
     "c1",
     "D1",
@@ -37,11 +49,35 @@ let labelArray = [
     "g3",
     "A3",
     "a3",
-    "B3"
+    "B3",
+    "C4",
+    "c4",
+    "D4",
+    "d4",
+    "E4",
+    "F4",
+    "f4",
+    "G4",
+    "g4",
+    "A4",
+    "a4",
+    "B4",
 ]
 
 let hertzArray = [
     0,
+    65,
+    69,
+    73,
+    77,
+    82,
+    87,
+    92,
+    98,
+    103,
+    110,
+    117,
+    123,
     131,
     139,
     147,
@@ -77,7 +113,19 @@ let hertzArray = [
     831,
     880,
     932,
-    988
+    988,
+    1046,
+    1108,
+    1174,
+    1245,
+    1319,
+    1397,
+    1480,
+    1568,
+    1661,
+    1760,
+    1865,
+    1976
 ]
 
 let timbreArray = [
@@ -566,6 +614,32 @@ namespace waves {
 
     let effectSlide = false
     let lastHertz= 0
+    let lastPlayedWasSlide = false
+
+    function resetVars(){
+        index = 6
+        loop = false
+        timbre = WaveShape.Square
+        hertzToPlay = 0
+
+        chordDistance = 0
+        chordIndex = 0
+        chordOrigin = 0
+        storedTimer = 0
+        checkChord = false
+
+        repeatOrigin = 0
+        repeatTimes = 0
+        checkRepeat = false
+
+        volumeStart = 255
+        volumeEnd = 255
+        volumePercent = 100
+
+        effectSlide = false
+        lastHertz = 0
+        lastPlayedWasSlide = false
+    }
 
     loops.everyInterval(1, songCycle)
 
@@ -582,11 +656,15 @@ namespace waves {
             if (labelArray.indexOf(content.substr(0, 2)) == -1) {
                 index += 3
                 if (content.charAt(0) == "$") {
-                    timbre = timbreArray.get(parseInt(content.charAt(1)))
+                    if (content.charAt(1) == "s"){
+                        effectSlide = (content.charAt(2) == "1")
+                    } else {
+                        timbre = timbreArray.get(parseInt(content.charAt(1)))
 
-                    let volumeString = volumeFxArray.get(parseInt(content.charAt(2)))
-                    volumeStart = parseInt(volumeString.substr(0, 3))
-                    volumeEnd = parseInt(volumeString.substr(4, 3))
+                        let volumeString = volumeFxArray.get(parseInt(content.charAt(2)))
+                        volumeStart = parseInt(volumeString.substr(0, 3))
+                        volumeEnd = parseInt(volumeString.substr(4, 3))
+                    }
                 } else if (content.charAt(0) == "%") {
                     checkChord = true
                     chordDistance = parseInt(content.charAt(1))
@@ -614,17 +692,6 @@ namespace waves {
                             volumePercent = 100
                         }
                     }
-                } else if (content.charAt(0) == "&"){
-                    let setThisTo = false
-                    if (content.charAt(2) == "1"){
-                        setThisTo = true
-                    } else {
-                        setThisTo = false
-                    }
-
-                    if (content.charAt(1) == "0") {
-                        effectSlide = setThisTo
-                    }
                 }
             } else {
 
@@ -639,16 +706,20 @@ namespace waves {
                 if (content.charAt(2) == "D"){
                     noteDurat = music.beat(BeatFraction.Double)
                 } else if (content.charAt(2) == "T") {
-                    noteDurat = music.beat(BeatFraction.Whole * 3)
+                    noteDurat = music.beat(BeatFraction.Whole) * 3
                 } else if (content.charAt(2) == "Q") {
                     noteDurat = music.beat(BeatFraction.Double) * 2
                 } else {
-                noteDurat = music.beat(BeatFraction.Whole) / parseInt(content.charAt(2))
+                    noteDurat = music.beat(BeatFraction.Whole) / parseInt(content.charAt(2))
                 }
 
                 if (effectSlide && (!(hertzToPlay == lastHertz)) && 29 < noteDurat){
                     noteDurat = 30
                     allowRepeatCode = false
+                    lastPlayedWasSlide = true
+                } else if (lastPlayedWasSlide){
+                    lastPlayedWasSlide = false
+                    noteDurat -= 30
                 }
 
                 if (checkChord) {
@@ -722,15 +793,13 @@ namespace waves {
             musicState = 1
             if (action == WavesActions.Restart) {
                 index = 6
-                checkChord = false
-                checkRepeat = false
+                resetVars()
             }
         } else {
             musicState = 0
             if (action == WavesActions.Stop) {
                 index = 6
-                checkChord = false
-                checkRepeat = false
+                resetVars()
             }
         }
     }
@@ -738,9 +807,12 @@ namespace waves {
     //% block="Store preset song $presetToStore"
     //% group="Store a song"
     export function storePreset(presetToStore: WavesPreset) {
-        sheet = sheet
+        if (presetToStore == WavesPreset.VivaldiSpring){
+            sheet = "000106$02@02E22g22g22g22f24E24%23g2.B2.B24A24g22g22g22f24E24%23g2.B2.B24A24g22A24B24A22g22%23f2.d2.f2.!50@..!00g22B22A24g24A22B22c32%22g2.B2.g22B22A24g24A22B22c32%22g2.B2.g22c32%22g2.B2.A22g22f24E24%32d2.g2.f2.%24f2.E2.!50g22B22A24g24A22B22c32%22g2.B2.g22B22A24g24A22B22c32%22g2.B2.g22c32%22g2.B2.A22g22f24E24%32d2.g2.f2.!00B28c38B24HH4B28c38B24HH2!50@02B28c38B24A24g24A24B28c38B24HH4@..HH4B22B28A28g28f28E22B22B22B28A28g28f28E22B22B24A24g24A24B28A28g24B24B24B24A24g24A24B28A28B24c34d34E34d34c34B24A24g24f24E24@02B22B28A28g28f28E22B22@..B22B28A28g28f28%21E2.B2.c34d34E34d34c34B24A24g24f24E24@02g22HH4A24@..%21d2.f2.g24A24g22HH4A24%24g2.A2.%34g2.A2.E3.%32g2.A2.B2.E34B24c34B24%32g2.A2.B2.@05E34B24c34B24@..%32g2.A2.B2.E34B24c34B24%32g2.A2.B2.@04E34B24c34B24@..E34B24c34E21HH4E34B24E24f24%32E2.E3.f3.E34B24E24f24%38E2.E3.f3.E31HH2!00g22B22A24g24A22B22c32%22g2.B2.g22B22A24g24A22B22c32%22g2.B2.g22c32%22g2.B2.A22g22f24E24%32d2.g2.f2.%24f2.E2.!50g22B22A24g24A22B22c32%22g2.B2.g22B22A24g24A22B22c32%22g2.B2.g22c32%22g2.B2.A22g22f24E24%32d2.g2.f2.!20g24A24B24A24B24A24g24A24!40g24A24B24A24B24A24g24A24!70g24A24B24A24B24A24g24A24!00g24A24B24c34B24c34!50d34E34d34E34f34E34f34E34!00d34E34d34E34f34g34f34g34!50A34g34A34g34f34A34g34f34%21E3.g3.!40g34E34d34c34@03g24A24B24A24B24A24g24A24@..%22E2.g2.HH2%21E3.g3.!00%34B2.f3.A3.%34B2.E3.g3.%34B2.d3.f3.%34B2.E3.g3.%34B2.f3.A3.%34B2.E3.g3.%34B2.d3.f3."
+        }
         music.setTempo(parseFloat(sheet.substr(3, 3)))
         sheetVersion = sheet.substr(0, 3)
+        resetVars()
     }
 
     //% block="Store custom song $sheetToStore"
@@ -763,6 +835,7 @@ namespace waves {
         // Set other variables
         music.setTempo(parseFloat(sheet.substr(3, 3)))
         sheetVersion = sheet.substr(0, 3)
+        resetVars()
     }
 
     //% block="Set loop to $loopBool"
